f0cd45be35c0502a96cb407deb606301ff7c71d9
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-23 11:20:33 +0200

    Update App.jsx

--- project-12/src/App.jsx
+++ project-12/src/App.jsx
@@ -2,25 +2,25 @@ import { Route, Routes } from "react-router-dom"
 import TaskList from "./pages/TaskList/TaskList"
 import TaskForm from "./pages/TaskForm/TaskForm"
 import Header from "./Components/Header/Header"
-import TaskItem from "./Components/TaskItem/TaskItem"
 import { TaskProvider } from "./Components/TaskContext/TaskContext"
 
 function App() {
 
 
   return (
-    <TaskProvider>
-    <div className="h-svh">
-    <Header />
-    <Routes>
-    <Route path="/" element={<TaskList />} />
-    <Route path="/project/:id" element={<TaskList />} />
-    <Route path="/task/:id" element={<TaskItem />} />
-    <Route path="/new-task" element={<TaskForm />} />
-    </Routes>
-    </div>
-    </TaskProvider>
-    
+
+ <TaskProvider>
+ <div className="h-svh">
+ <Header />
+ <Routes>
+ <Route path="/" element={<TaskList />} />
+ <Route path="/project/:id" element={<TaskList />} />
+ <Route path="/new-task" element={<TaskForm />} />
+ </Routes>
+ </div>
+ </TaskProvider>
+ 
+
   )
 } 
 
