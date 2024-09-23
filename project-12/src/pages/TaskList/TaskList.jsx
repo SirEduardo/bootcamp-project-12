8edc339aa06ccc871cd4f0c8c7444ee634e0ee82
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-23 11:20:55 +0200

    Update TaskList.jsx

--- project-12/src/pages/TaskList/TaskList.jsx
+++ project-12/src/pages/TaskList/TaskList.jsx
@@ -1,35 +1,52 @@
-import {  useContext } from "react";
-import { TaskCards } from "../../Components/TaskCards/TaskCards";
+import { useContext } from "react";
 import { TaskContext } from "../../Components/TaskContext/TaskContext";
-
+import { TaskSection } from "../../Components/TaskSection/TaskSection";
 
 const TaskList = () => {
-  const { tasks, deleteTask } = useContext(TaskContext)
+  console.log("soy la lista y me renderizo");
+  
+  const { tasks, deleteTask, updateTaskStatus } = useContext(TaskContext);
 
   if (!Array.isArray(tasks)) {
-    return <p className="text-center text-gray-500">Error: Las tareas no se han cargado correctamente.</p>;
+    return (
+      <p className="text-center text-gray-500">
+        Error: Las tareas no se han cargado correctamente.
+      </p>
+    );
   }
 
-  return (
-    <div className="h-svh bg-slate-300 flex justify-center gap-5 pt-10">
-    {tasks.length === 0 ? (
-      <p className="text-center text-gray-500">No hay tareas disponibles</p>
-    ) : (
-      tasks.map((task) => (
-        <TaskCards
-          key={task.id}
-          title={task.title}
-          tag={task.tag}
-          priority={task.priority}
-          date={task.date}
-          onDelete={() => deleteTask(task.id)}
-        />
+  const notStartedTasks = tasks.filter((task) => task.status === "not_started");
+  const inProgressTasks = tasks.filter((task) => task.status === "in_progress");
+  const completedTasks = tasks.filter((task) => task.status === "completed");
 
-      ))
-    )}
-    
+  return (
+    <div className="min-h-screen bg-slate-300 flex justify-around gap-5 pt-10 pb-10">
+      <TaskSection
+        title="Tareas"
+        tasks={notStartedTasks}
+        status="not_started"
+        color="border-black"
+        updateTaskStatus={updateTaskStatus}
+        deleteTask={deleteTask}
+      />
+      <TaskSection
+        title="Tareas en Progreso"
+        tasks={inProgressTasks}
+        status="in_progress"
+        color="border-yellow-600 text-yellow-600"
+        updateTaskStatus={updateTaskStatus}
+        deleteTask={deleteTask}
+      />
+      <TaskSection
+        title="Tareas Completadas"
+        tasks={completedTasks}
+        status="completed"
+        color="border-green-500 text-green-500"
+        updateTaskStatus={updateTaskStatus}
+        deleteTask={deleteTask}
+      />
     </div>
   );
 };
 
-export default TaskList;
+export default TaskList
