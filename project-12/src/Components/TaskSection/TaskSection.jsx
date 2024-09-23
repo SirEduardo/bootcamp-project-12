552bf9e85ca182eeca5da12f9b92799cd311dfec
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-23 11:20:45 +0200

    Create TaskSection.jsx

---
+++ project-12/src/Components/TaskSection/TaskSection.jsx
@@ -0,0 +1,23 @@
+import { Droppable } from "../../Components/Droppable/Droppable";
+import { TaskCards } from "../../Components/TaskCards/TaskCards";
+
+export const TaskSection = ({ title, tasks, status, color, updateTaskStatus, deleteTask }) => (
+    <div className="flex flex-col pl-3 gap-5 pt-10 text-center">
+      <h2 className={`text-4xl border-b-2 ${color}`}>
+        {title}
+      </h2>
+      <Droppable status={status} onDrop={updateTaskStatus}>
+        {tasks.length === 0 ? (
+          <p className="text-center text-gray-500">No hay tareas {title.toLowerCase()}</p>
+        ) : (
+          tasks.map((task) => (
+            <TaskCards
+              key={task.id}
+              task={task}
+              onDelete={() => deleteTask(task.id)}
+            />
+          ))
+        )}
+      </Droppable>
+    </div>
+  );
