67ca8523787dc3be6aa1155c66e9b43d88d9437b
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-02 15:16:23 +0200

    Update Header.jsx

--- project-12/src/Components/Header/Header.jsx
+++ project-12/src/Components/Header/Header.jsx
@@ -2,31 +2,43 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="h-20 bg-slate-500 flex items-center justify-center">
-      <nav className="w-full">
-        <ul className="w-full flex justify-evenly font-semibold">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "border-b-2 border-black" : ""
-              }
-            >
-              Task List
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/new-task"
-              className={({ isActive }) =>
-                isActive ? "border-b-2 border-black" : ""
-              }
-            >
-              New Task
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-gradient-to-r from-purple-500 to-indigo-600 shadow-lg">
+      <div className="container mx-auto px-4">
+        <nav className="h-20 flex items-center justify-center">
+          <ul className="flex space-x-8">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `text-white font-semibold text-lg transition duration-300 ease-in-out
+               ${
+                 isActive
+                   ? "border-b-2 border-white"
+                   : "hover:text-purple-200 hover:border-b-2 hover:border-purple-200"
+               }`
+                }
+              >
+                Lista de Tareas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/new-task"
+                className={({ isActive }) =>
+                  `text-white font-semibold text-lg transition duration-300 ease-in-out
+                ${
+                  isActive
+                    ? "border-b-2 border-white"
+                    : "hover:text-purple-200 hover:border-b-2 hover:border-purple-200"
+                }`
+                }
+              >
+                Nueva Tarea
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
